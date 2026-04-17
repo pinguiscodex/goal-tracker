0da74d121a18f52e919bb3b46cd6a0d5526fa798
pinguiscodex <pengu@sky-network.org>
2026-04-17 16:13:28 +0200

    Initialized project, backend + frontend (setup templates)

---
+++ app/frontend/src/app/login/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "../globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Goal Tracker",
+  description: "Track the goal counts of your matches",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html suppressHydrationWarning lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
